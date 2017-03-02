7d552cdb6075102f814d1cfb0fb13ec281448b5a
Jimmy DESMOT <E154890J@U-INF-J-C002-04.iut-nantes.univ-nantes.prive>
2017-03-02 11:17:44 +0100

    insertion des données dans la base de données

--- extraction_csv.py
+++ extraction_csv.py
@@ -1,6 +1,37 @@
 import csv
+import sqlite3
+
+conn = sqlite3.connect('fichierDonnees')
+cur = conn.cursor()
+
+file = open("res/23440003400026_J335_installations_table.csv","r")
+test = csv.reader(file)
+for row in test:
+    numero=row[1]
+    nom=row[0]
+    ville=row[2]
+    latitude=row[10]
+    longitude=row[9]
+    adresse=(row[5]+''+row[6]+''+row[7])
+    cp=row[4]
+    cur.execute("INSERT INTO Installation(numero,nom,ville,latitude,longitude,adresse,code_postal) VALUES(?,?,?,?,?,?,?);", (numero,nom,ville,latitude,longitude,adresse,cp))
+
+file = open("res/J334_equipements_activites.csv","r")
+test = csv.reader(file)
+for row in test:
+    code=row[4]
+    nom=row[5]
+    equipementId=row[2]
+    cur.execute("INSERT INTO Activité(code,nom,equipementId) VALUES(?,?,?);", (code,nom,equipementId))
 
 file = open("res/equipements.csv","r")
 test = csv.reader(file)
 for row in test:
-    if row[1]=="Geneston": print(row[3])
+    id=row[4]
+    nom=row[3]
+    InsNumeroInstall=row[2]
+    cur.execute("INSERT INTO Equipement(id,nom,InsNumeroInstall) VALUES(?,?,?);", (id,nom,InsNumeroInstall))
+
+conn.commit()
+cur.close()
+conn.close()
