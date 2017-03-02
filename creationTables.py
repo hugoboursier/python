acc7748c4c2b9ccfbcfbd14e1b14bb2398644591
Hugo BOURSIER <E155590U@U-INF-J-C002-05.iut-nantes.univ-nantes.prive>
2017-03-02 10:14:37 +0100

    fichier créant les 3 tables

---
+++ creationTables.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+fichierDonnees ="/hometu/etudiants/b/o/E155590U/2eme_annee/python/python/db.sq3"
+conn = sqlite3.connect('fichierDonnees')
+cur = conn.cursor()
+cur.execute("CREATE TABLE Installation (numero INTEGER, nom TEXT, ville TEXT, latitude REAL, longitude REAL, adresse TEXT, code_postal INTEGER)")
+cur.execute("CREATE TABLE Equipement (id INTEGER, nom TEXT, InsNumeroInstall INTEGER)")
+cur.execute("CREATE TABLE Activité (code INTEGER, nom TEXT, equipementId INTEGER)")
