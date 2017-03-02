0f422fc89517c58b7d35a46e0604934d27b4161a
Hugo BOURSIER <E155590U@U-INF-J-C002-05.iut-nantes.univ-nantes.prive>
2017-03-02 11:45:06 +0100

    fichier créant les tables

--- creationTables.py
+++ creationTables.py
@@ -3,9 +3,17 @@ import sqlite3
 fichierDonnees ="/hometu/etudiants/b/o/E155590U/2eme_annee/python/python/db.sq3"
 conn = sqlite3.connect('fichierDonnees')
 cur = conn.cursor()
-cur.execute("CREATE TABLE Installation (numero INTEGER, nom TEXT, ville TEXT, latitude REAL, longitude REAL, adresse TEXT, code_postal INTEGER)")
-cur.execute("CREATE TABLE Equipement (id INTEGER, nom TEXT, InsNumeroInstall INTEGER)")
-cur.execute("CREATE TABLE Activité (code INTEGER, nom TEXT, equipementId INTEGER)")
+"""
+cur.execute("drop table Installation")
+cur.execute("drop table Equipement")
+cur.execute("drop table Activité")
+"""
+cur.execute("CREATE TABLE Installation (numero INTEGER PRIMARY KEY, nom TEXT, ville TEXT, latitude REAL, longitude REAL, adresse TEXT, code_postal INTEGER)")
+cur.execute("CREATE TABLE Equipement (id INTEGER PRIMARY KEY, nom TEXT, insNumeroInstall INTEGER,FOREIGN KEY(insNumeroInstall) REFERENCES Installation(numero))")
+cur.execute("CREATE TABLE Activité (code INTEGER, nom TEXT, equipementId INTEGER,FOREIGN KEY(equipementId) REFERENCES Equipement(id),PRIMARY KEY(code,equipementId))")
+
+cur.execute("CREATE TABLE Equip_Act(idEquip INTEGER,codeAct INTEGER,PRIMARY KEY(idEquip,codeAct))")
+
 conn.commit()
 cur.close()
 conn.close()
