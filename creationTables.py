ecf7972cce068c610001602ebf1e700e5496ad2e
Jimmy DESMOT <E154890J@U-INF-J-C002-19.iut-nantes.univ-nantes.prive>
2017-03-09 10:04:00 +0100

    mise-à-jour des fichier creation table et extraction_csv

--- creationTables.py
+++ creationTables.py
@@ -1,13 +1,15 @@
 import sqlite3
 
-fichierDonnees ="/hometu/etudiants/b/o/E155590U/2eme_annee/python/python/db.sq3"
+fichierDonnees ="/hometu/etudiants/d/e/E154890J/Bureau/Python/python/db.sq3"
 conn = sqlite3.connect('fichierDonnees')
 cur = conn.cursor()
 """
 cur.execute("drop table Installation")
 cur.execute("drop table Equipement")
 cur.execute("drop table Activité")
+cur.execute("drop table Equip_Act")
 """
+
 cur.execute("CREATE TABLE Installation (numero INTEGER PRIMARY KEY, nom TEXT, ville TEXT, latitude REAL, longitude REAL, adresse TEXT, code_postal INTEGER)")
 cur.execute("CREATE TABLE Equipement (id INTEGER PRIMARY KEY, nom TEXT, insNumeroInstall INTEGER,FOREIGN KEY(insNumeroInstall) REFERENCES Installation(numero))")
 cur.execute("CREATE TABLE Activité (code INTEGER, nom TEXT, equipementId INTEGER,FOREIGN KEY(equipementId) REFERENCES Equipement(id),PRIMARY KEY(code,equipementId))")
--- extraction_csv.py
+++ extraction_csv.py
@@ -7,29 +7,31 @@ cur = conn.cursor()
 file = open("res/23440003400026_J335_installations_table.csv","r")
 test = csv.reader(file)
 for row in test:
-    numero=row[1]
+    numero=int(row[1])
     nom=row[0]
     ville=row[2]
-    latitude=row[10]
-    longitude=row[9]
+    latitude=float(row[10])
+    longitude=float(row[9])
     adresse=(row[5]+''+row[6]+''+row[7])
-    cp=row[4]
+    if row[4]!='' :
+        cp=int(row[4])
     cur.execute("INSERT INTO Installation(numero,nom,ville,latitude,longitude,adresse,code_postal) VALUES(?,?,?,?,?,?,?);", (numero,nom,ville,latitude,longitude,adresse,cp))
 
 file = open("res/J334_equipements_activites.csv","r")
 test = csv.reader(file)
 for row in test:
-    code=row[4]
+    code=int(row[4])
     nom=row[5]
-    equipementId=row[2]
+    equipementId=int(row[2])
+    print(String(code)+' '+String(equipementId))
     cur.execute("INSERT INTO Activité(code,nom,equipementId) VALUES(?,?,?);", (code,nom,equipementId))
 
 file = open("res/equipements.csv","r")
 test = csv.reader(file)
 for row in test:
-    id=row[4]
+    id=int(row[4])
     nom=row[3]
-    InsNumeroInstall=row[2]
+    InsNumeroInstall=int(row[2])
     cur.execute("INSERT INTO Equipement(id,nom,InsNumeroInstall) VALUES(?,?,?);", (id,nom,InsNumeroInstall))
 
 conn.commit()
