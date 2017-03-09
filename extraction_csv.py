9ef7aa2c301ed0508feebc67075b1ea265773744
Jimmy DESMOT <E154890J@U-INF-J-C002-19.iut-nantes.univ-nantes.prive>
2017-03-09 11:36:42 +0100

    problème résolue sur extraction_csv

--- extraction_csv.py
+++ extraction_csv.py
@@ -15,34 +15,25 @@ for row in test:
     adresse=(row[5]+''+row[6]+''+row[7])
     if row[4]!='' :
         cp=int(row[4])
-    """cur.execute("INSERT INTO Installation(numero,nom,ville,latitude,longitude,adresse,code_postal) VALUES(?,?,?,?,?,?,?);", (numero,nom,ville,latitude,longitude,adresse,cp))"""
+    cur.execute("INSERT INTO Installation(numero,nom,ville,latitude,longitude,adresse,code_postal) VALUES(?,?,?,?,?,?,?);", (numero,nom,ville,latitude,longitude,adresse,cp))
 
 file = open("res/J334_equipements_activites.csv","r")
 test = csv.reader(file)
 
-tmp1=0
-tmp2=0
 for row in test:
+    tmp ="vide"
     if row[4]!='' :
         code=int(row[4])
     nom=row[5]
     equipementId=int(row[2])
-    """if cur.execute("SELECT code, equipementId FROM Activité WHERE code = ? and equipementId = ?;", (code,equipementId, ))==None:"""
+
     cur.execute("SELECT * FROM Activité WHERE code = ? and equipementId = ?;", (code,equipementId))
     rows = cur.fetchall();
     for row in rows:
-        print(row)
-    cur.execute("INSERT INTO Activité(code,nom,equipementId) VALUES(?,?,?);", (code,nom,equipementId))
-    """if tmp1==0 and tmp2==0 :
+        tmp = str(row)
+
+    if(tmp=="vide") :
         cur.execute("INSERT INTO Activité(code,nom,equipementId) VALUES(?,?,?);", (code,nom,equipementId))
-    elif tmp1!=code and tmp2!=equipementId :
-        cur.execute("INSERT INTO Activité(code,nom,equipementId) VALUES(?,?,?);", (code,nom,equipementId))
-    cur.execute("SELECT * from Activité;")
-    rows = cur.fetchall();
-    print(str(code)+' '+str(equipementId))
-    for row in rows:
-        tmp1 = row[0]
-        tmp2 = row[2]"""
 
 file = open("res/equipements.csv","r")
 test = csv.reader(file)
@@ -50,7 +41,7 @@ for row in test:
     id=int(row[4])
     nom=row[3]
     InsNumeroInstall=int(row[2])
-    """cur.execute("INSERT INTO Equipement(id,nom,InsNumeroInstall) VALUES(?,?,?);", (id,nom,InsNumeroInstall))"""
+    cur.execute("INSERT INTO Equipement(id,nom,InsNumeroInstall) VALUES(?,?,?);", (id,nom,InsNumeroInstall))
 
 conn.commit()
 cur.close()
