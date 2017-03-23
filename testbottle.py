463edfea25a2e931c27afac99aee96524029fcb5
Jimmy DESMOT <E154890J@U-INF-J-C002-19.iut-nantes.univ-nantes.prive>
2017-03-23 11:21:45 +0100

    methode pour select ville et activite

--- testbottle.py
+++ testbottle.py
@@ -1,7 +1,29 @@
-from bottle import route, run, template
+from lib.bottle import route, run, template
+import sqlite3
 
-@route('/hello/<name>')
+
+@route('/ville/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    conn = sqlite3.connect('fichierDonnees')
+    cur = conn.cursor()
+    cur.execute("SELECT nom FROM Installation WHERE ville = ?;", (name, ))
+    rows = cur.fetchall()
+    stre = ""
+    for row in rows:
+        stre = stre + str(row) + "<br>"
 
-run(host='localhost', port=8080)
+    return stre
+
+@route('/activite/<name>')
+def index(name):
+    conn = sqlite3.connect('fichierDonnees')
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT i.nom, i.ville FROM Installation i,Equipement e,Activité a WHERE i.numero=e.insNumeroInstall and e.id=a.equipementId and a.nom=?", (name, ))
+    rows = cur.fetchall()
+    stre = ""
+    for row in rows:
+        stre = stre + str(row) + "<br>"
+
+    return stre
+
+run(host='localhost', port=8888)
